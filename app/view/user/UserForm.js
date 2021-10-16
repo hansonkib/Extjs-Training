393eb9f7de7a1c0bc52c06ccab65424aaf01c432
hansonkib <hansonkib@gmail.com>
2021-10-16 03:57:01 +0300

    modified user form

--- app/view/user/UserForm.js
+++ app/view/user/UserForm.js
@@ -2,41 +2,85 @@ Ext.define('TrainingApp.view.user.UserForm', {
     extend: 'Ext.window.Window',
     xtype: 'userform',
     controller: 'userformcontroller',
+    // height: 300,
+    // width: 900,
+    layout: {
+        type: 'fit'
+    },
+    bind: {
+        title: '{title}'
+    },
+    closable: true,
+    modal: true,
     autoShow: true,
+
     items: [{
         xtype: 'form',
         reference: 'add-form',
+        bodyPadding: 5,
         jsonSubmit: true,
         defaults: {
             anchor: '100%'
         },
-        //content of the form
-        items: [{
-            xtype: 'numberfield',
-            name: 'id',
-            fieldLabel: 'Student ID',
-            minValue: 0,
-            readOnly: true,
+        layout: {
+            type: 'hbox',
+            align: 'stretch'
         },
+        //content of the form
+        items: [
             {
-                xtype: 'textfield',
-                name: 'name',
-                fieldLabel: 'Name'
+                xtype: 'fieldset',
+                title: 'User Information',
+                flex: 1,
+                layout: 'anchor',
+                defaults: {
+                    anchor: '100%',
+                    xtype: 'textfield',
+                    msgTarget: 'side',
+                    labelWidth: 75
+                },
+                items: [
+                    {
+                        xtype: 'numberfield',
+                        name: 'id',
+                        fieldLabel: 'User ID',
+                        minValue: 0,
+                        readOnly: true,
+                    },
+                    {
+                        xtype: 'textfield',
+                        name: 'name',
+                        fieldLabel: 'Name'
+                    },
+                    {
+                        xtype: 'textfield',
+                        name: 'company.companyName',
+                        fieldLabel: 'Company Name'
+                    },
+                    {
+                        xtype: 'textfield',
+                        name: 'address.suite',
+                        fieldLabel: 'Suite'
+                    },
+                    {
+                        xtype: 'textfield',
+                        name: 'address.geo.lat',
+                        fieldLabel: 'Lat'
+                    },
+                ]
             },
-
+            //second fieldset
             {
-                xtype: 'textfield',
-                name: 'company.companyName',
-                fieldLabel: 'Company Name'
-            },
-            {
-                xtype: 'textfield',
-                name: 'address.suite',
-                fieldLabel: 'Suite'
-            }, , {
-                xtype: 'textfield',
-                name: 'address.geo.lat',
-                fieldLabel: 'Lat'
+                xtype: 'fieldset',
+                title: 'Photo',
+                width: 170,
+                items: [
+                    {
+                        xtype: 'image',
+                        height: 150,
+                        width: 150,
+                    }
+                ]
             }
         ],
         buttons: [{
